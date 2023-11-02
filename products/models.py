2b01fd0111910f579c51d412b63c23ff9f75608b
Niraj Shrestha <nshrest23@Nirajs-MacBook-Air.local>
2023-11-02 08:08:34 +0545

    day17 - JWT & Swagger setup

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    price = models.FloatField(default=0)
+    quantity = models.IntegerField(default=0)
+    product_img = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'product'
+    
+    def __str__(self):
+        return self.title
--- products/views.py
+++ products/views.py
@@ -1,9 +1,11 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 import rest_framework.status as api_status
 from django.http import JsonResponse, HttpResponse
+from rest_framework.permissions import IsAuthenticated
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def home(request):
     name = request.GET.get("name")
     if name:
